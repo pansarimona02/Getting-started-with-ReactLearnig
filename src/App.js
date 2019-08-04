6d37cd43ca7a080537fc052ff52c9ceed49b880a
pansarimona02 <MonaKumari@fico.com>
2019-08-04 15:55:07 +0530

    Functional Component and use of hooks(useState)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-function App() {
-    return(
+
+class App extends Component { //prior 16.8 only way to create class.
+//INTRODUCING state : Special properties for component only
+  state = {
+    persons: [
+      {name: "monika", age: 30},
+      {name: "anamika", age: 20}
+    ] 
+  }
+  switchNameHandler= () => {
+    this.setState({
+      persons: [{name: "komal", age : 21},
+      {name:"shweta", age: 26}]
+  })
+  }
+  render() {
+    return (
       <div className="App">
         <h1>HI</h1>
         <p>How Are You?</p>
-        <Person  name="mona" age="24">Hi I cut cake on 2 of August</Person>
+        <button onClick={this.switchNameHandler}>SWITCH NAME</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hi I cut cake on 2 of August</Person>
       </div>
-    ); 
-    //return React.createElement('div', {className : "App"},React.createElement('h1',null,'hi, how are you?'));
+    );
+  }
 
-
+  //return React.createElement('div', {className : "App"},React.createElement('h1',null,'hi, how are you?'));
 
 }
+
+
 
 export default App;
---
+++ src/FunctionalApp.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+// Alternative way for Aap.js class
+const FunctionalApp = (props) => {
+// after ver 16.8
+//INTRODUCING state : Special properties for component only
+const [ personState, setPersonState ] = useState({
+  persons: [
+    {name: "monika", age: 30},
+    {name: "anamika", age: 20}
+  ] 
+});
+
+
+ const switchNameHandler = () => {
+    setPersonState({
+      persons: [{name: "komalika", age : 21},
+      {name:"shweta", age: 26}]
+  });
+  };
+
+    return (
+      <div className="App">
+        <h1>HI</h1>
+        <p>How Are You?</p>
+        <button onClick={switchNameHandler}>SWITCH NAME</button>
+        <Person name={personState.persons[0].name} age={personState.persons[0].age}>Hi I cut cake on 2 of August</Person>
+      </div>
+    )
+  
+
+  //return React.createElement('div', {className : "App"},React.createElement('h1',null,'hi, how are you?'));
+
+};
+export default FunctionalApp;
