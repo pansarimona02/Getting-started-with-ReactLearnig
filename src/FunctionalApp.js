2b349047aa95ba01484644599052ec841a741b0d
pansarimona02 <MonaKumari@fico.com>
2019-08-07 09:40:24 +0530

    passing function as a refference

--- src/FunctionalApp.js
+++ src/FunctionalApp.js
@@ -22,11 +22,11 @@ const [ personState, setPersonState ] = useState({
   };
 
     return (
-      <div className="App">
+      <div className="App">git
         <h1>HI</h1>
         <p>How Are You?</p>
         <button onClick={switchNameHandler}>SWITCH NAME</button>
-        <Person name={personState.persons[0].name} age={personState.persons[0].age}>Hi I cut cake on 2 of August</Person>
+        <Person click= {switchNameHandler} name={personState.persons[0].name} age={personState.persons[0].age}>Hi I cut cake on 2 of August</Person>
       </div>
     )
   
